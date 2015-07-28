12cb93fd450e77aa8a3107fc9cc43f3c679fb114
Your Name <you@example.com>
2015-07-28 21:08:03 +0900

    update

---
+++ Joystick_get.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "joystick.h"
+#include <fcntl.h>
+#define BUTTON_DATA_MAX 12
+#define STICK_DATA_MAX 6
+
+void dbg_cmd_view(unsigned char btn[],signed int stk[]){
+    int i;
+    for(i = 0;i < BUTTON_DATA_MAX;i++){
+        printf("%3d:",btn[i]);
+    }
+    printf("\n");
+    for(i = 0;i < STICK_DATA_MAX;i++){
+        printf("%3d:",stk[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+        
+    int fd;
+    fd = open( "/dev/input/js0", O_RDONLY );
+
+    unsigned char  ButtonData[BUTTON_DATA_MAX];
+    signed int     StickData[STICK_DATA_MAX];
+
+    for(;;){
+        struct js_event  event;
+        if( read( fd, &event, sizeof(struct js_event) ) >= sizeof(struct js_event) ){
+            switch( event.type & 0x7f ){
+            case JS_EVENT_BUTTON:
+                if( event.number < BUTTON_DATA_MAX ){
+                    ButtonData[ event.number ]= event.value;
+                }
+                break;
+            case JS_EVENT_AXIS:
+                if( event.number < STICK_DATA_MAX ){
+                    StickData[ event.number ]= event.value;
+                }
+                break;
+            }
+        }
+        dbg_cmd_view(ButtonData,StickData);       //debug
+
+    }
+    close( fd );
+
+    return 0;
+}
+
+
---
+++ upload.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+rm a.out
+git add .
+
+git commit -m "update"
+
+git push
+
